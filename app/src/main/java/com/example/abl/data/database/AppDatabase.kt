f0170048488eed568ba5c0ed066f886e0a6e8646
Romeo <popescuromeo22@stud.ase.ro>
2025-03-29 09:14:53 +0200

    Added AppDatabase.kt file, finished the database

---
+++ app/src/main/java/com/example/abl/data/database/AppDatabase.kt
@@ -0,0 +1,31 @@
+package com.example.abl.data.database
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.example.abl.data.database.dao.AppInformationDao
+import com.example.abl.data.database.dao.AppUsageDataDao
+import com.example.abl.data.database.dao.AppUsagePatternDao
+import com.example.abl.data.database.dao.RecommendationsDao
+import com.example.abl.data.database.dao.RiskyAppDao
+import com.example.abl.data.database.dao.UserProfileDao
+import com.example.abl.data.database.entity.AppInformation
+import com.example.abl.data.database.entity.AppUsageData
+import com.example.abl.data.database.entity.AppUsagePattern
+import com.example.abl.data.database.entity.Recommendations
+import com.example.abl.data.database.entity.RiskyApp
+import com.example.abl.data.database.entity.UserProfile
+
+@Database(
+    entities = [AppInformation::class, AppUsageData::class,
+        AppUsagePattern::class, Recommendations::class,
+        RiskyApp::class, UserProfile::class],
+    version = 1
+)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun appInformationDao(): AppInformationDao
+    abstract fun appUsageDataDao(): AppUsageDataDao
+    abstract fun appUsagePatternDao(): AppUsagePatternDao
+    abstract fun recommendationsDao(): RecommendationsDao
+    abstract fun riskyAppDao(): RiskyAppDao
+    abstract fun userProfileDao(): UserProfileDao
+}
